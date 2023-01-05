5d18db49e62dbcde5659417852ecde1437cd3f03
Kashif Ali <102131005+Kashif27242@users.noreply.github.com>
2023-01-05 17:31:59 +0500

    Add files via upload

---
+++ Textutlis/index.js
@@ -0,0 +1,12 @@
+function ok() {
+
+    // var text = document.getElementById("text").value;
+    // var uptext = text.toUpperCase();
+    document.getElementById("me").innerHTML = '<h1>$${uptext}</h1>' ;
+    console.log("btn is clicked")
+    
+
+}
+
+
+//$${}
